69eb08a99e50b4220ff9475528acfe40dd077fa9
Jan Heil <jan.heil@byte-store.de>
2024-12-27 10:53:20 +0100

    Refactor JMIXUpdateManager instantiation to include repositories
    Updated the JMIXUpdateManager initialization to include a new ArrayList for UpdateRepository objects. This change ensures the update manager is prepared to handle repositories upon creation. Additional import statements were added to support the modification.

--- plugin/src/main/java/de/bytestore/plugin/configuration/SpringUpdateManagerConfiguration.java
+++ plugin/src/main/java/de/bytestore/plugin/configuration/SpringUpdateManagerConfiguration.java
@@ -1,8 +1,11 @@
 package de.bytestore.plugin.configuration;
 
+import org.pf4j.update.UpdateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 @Component
 public class SpringUpdateManagerConfiguration {
@@ -11,6 +14,6 @@ public class SpringUpdateManagerConfiguration {
 
     @Bean
     public JMIXUpdateManager updateManager() {
-        return new JMIXUpdateManager(managerIO);
+        return new JMIXUpdateManager(managerIO, new ArrayList<UpdateRepository>());
     }
 }
